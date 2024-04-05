12d8cbffa373c8f020809d945bf78d923c92b778
Thiago Allan <thiagoallan864@gmail.com>
2024-04-05 09:09:08 -0300

    Aterações na pagina web

--- main.js
+++ main.js
@@ -1,9 +1,36 @@
-window.onscroll = function () {
+window.onScroll = function () {
   var navigation = document.getElementById('navigation')
   if (window.scrollY > 0) {
-    navigation.classList.add('scroll');
-    
+    navigation.classList.add('scroll')
   } else {
-    navigation.classList.remove('scroll');
+    navigation.classList.remove('scroll')
   }
 }
+
+function openMenu() {
+  document.body.classList.add('menu-expanded')
+}
+function closeMenu() {
+  document.body.classList.remove('menu-expanded')
+}
+
+ScrollReveal({
+  origin: 'top',
+  distance: '30px',
+  duration: 800
+})
+
+ScrollReveal().reveal(`
+  #home,
+  #home .what,
+  #home .gifSerra,
+  #home .container, 
+  #home .stats, 
+  #services,
+  #services header,
+  #services .card,#about 
+  .wrapper, 
+  .content, 
+  .galpao
+  
+`)
